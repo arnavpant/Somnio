c6c119df5daf9ce97c72dc9f30c5ba821839c018
arnavpant <69646164+arnavpant@users.noreply.github.com>
2025-03-04 08:52:13 +0000

    added changes to home page and made page.module.css file for styles in styles directory

--- somnio_frontend/src/app/page.js
+++ somnio_frontend/src/app/page.js
@@ -5,13 +5,13 @@ import styles from "./styles/page.module.css";
 export default function HomePage() {
   return (
     <div className={styles.container}>
-      <h1>SOMNIO</h1>
+      <h1 className={styles.title}>SOMNIO</h1>
       <div className={styles.buttonContainer}>
         <Link href="/login" legacyBehavior>
           <a className={styles.button}>Login</a>
         </Link>
         <Link href="/signup" legacyBehavior>
-          <a className={styles.button}>Get Started</a>
+          <a className={styles.button}>Sign Up</a>
         </Link>
       </div>
     </div>
